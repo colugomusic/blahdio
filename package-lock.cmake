bb7a0e572d1f06eed3518a7adb500c21dfa04f69
Colugo <colugomusic@gmail.com>
2021-05-06 15:09:27 +0100

    Update package versions

--- package-lock.cmake
+++ package-lock.cmake
@@ -1,28 +1,30 @@
 # CPM Package Lock
 # This file should be committed to version control
 
-# dr_libs (unversioned)
-# CPMDeclarePackage(dr_libs
-#  GIT_TAG master
-#  GITHUB_REPOSITORY mackron/dr_libs
-#  EXCLUDE_FROM_ALL YES
-#)
-# miniaudio (unversioned)
-# CPMDeclarePackage(miniaudio
-#  GIT_TAG master
-#  GITHUB_REPOSITORY mackron/miniaudio
-#  EXCLUDE_FROM_ALL YES
-#)
+# dr_libs
+CPMDeclarePackage(dr_libs
+  NAME dr_libs
+  GIT_TAG 343aa92
+  DOWNLOAD_ONLY YES
+  GITHUB_REPOSITORY mackron/dr_libs
+)
+# miniaudio
+CPMDeclarePackage(miniaudio
+  NAME miniaudio
+  GIT_TAG 199d6a7
+  DOWNLOAD_ONLY YES
+  GITHUB_REPOSITORY mackron/miniaudio
+)
 # utfcpp (unversioned)
 # CPMDeclarePackage(utfcpp
 #  NAME utfcpp
-#  GIT_TAG master
+#  GIT_TAG ddd38b3
 #  DOWNLOAD_ONLY YES
 #  GITHUB_REPOSITORY nemtrif/utfcpp
 #)
-# wavpack (unversioned)
-# CPMDeclarePackage(wavpack
-#  GIT_TAG master
-#  GITHUB_REPOSITORY colugomusic/wavpack
-#  EXCLUDE_FROM_ALL YES
-#)
+# wavpack
+CPMDeclarePackage(wavpack
+  NAME wavpack
+  GIT_TAG 7349945
+  GITHUB_REPOSITORY colugomusic/wavpack
+)
